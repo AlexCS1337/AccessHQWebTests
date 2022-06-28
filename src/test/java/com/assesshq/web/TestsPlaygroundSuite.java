e6bb13ce5d2ae5835f904ad99ed2375df07c50e9
Alexander Silva <alexander.carlos.silva@gmail.com>
2022-06-28 14:16:00 +1000

    Update Verify_Message_Alert Test Method

--- src/test/java/com/assesshq/web/TestsPlaygroundSuite.java
+++ src/test/java/com/assesshq/web/TestsPlaygroundSuite.java
@@ -1,19 +1,34 @@
 package com.assesshq.web;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestsPlaygroundSuite {
 
-    @Test
-    public void Verify_Message_Alert() {
-        ChromeDriver driver = new ChromeDriver();
+    private ChromeDriver driver;
+
+    @BeforeEach
+    public void setupDriver() {
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
 
         driver.get("https://d18u5zoaatmpxx.cloudfront.net/");
+    }
+
+    @Test
+    public void Verify_Message_Alert() throws InterruptedException {
         driver.findElementById("forename").sendKeys("Test123");
         driver.findElementById("submit").click();
 
+        var popupElement = driver.findElementByClassName("popup-message");
+        new WebDriverWait(driver, 1).until(ExpectedConditions.visibilityOf(popupElement));
+        Assertions.assertEquals("Hello Test123", popupElement.getText());
+    }
+
+    @AfterEach
+    public void tearDown () {
         driver.quit();
     }
 }
